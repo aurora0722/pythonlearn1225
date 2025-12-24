0a37bd6595eeae109d43d4457cd86e9ba94b0eae
Swathi S <swathis.24csd@sonatech.ac.in>
2025-12-24 22:49:16 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -18,5 +18,10 @@ print("hello world")
 print("hello world")
 print("hello world")
 print("hello world")
->>>>>>> 39995d7eafcf1d88da949892739e74b12cfdb747
-totally thotale patasa na vedikura 10k wala hei 
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
